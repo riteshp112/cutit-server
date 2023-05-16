d1f4af008880ac7620cf3cac558c04e6c4350fef
Ritesh <ritesh.patel@daffodilsw.com>
2023-05-16 17:11:39 +0530

    config

--- App.py
+++ App.py
@@ -4,7 +4,17 @@ import pymongo
 from config import MONGO_DATABASE, MONGO_URI
 import random
 import string
-import validators
+import re
+regex = re.compile(
+        # r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+print(re.match(regex, "http://www.example.com") is not None) # True
+print(re.match(regex, "example.com") is not None) 
 client = pymongo.MongoClient(
     MONGO_URI,
     connect=False,
@@ -50,7 +60,7 @@ def hash_generator():
 def get_hash():
     try:
         jsonRequest =request.get_json(force=True)
-        if jsonRequest["url"] and validators.url(jsonRequest["url"]):
+        if jsonRequest["url"] and re.match(regex, jsonRequest["url"]) is not None:
             url = jsonRequest["url"]
             url = url.lower()
             urlres = db.urls.find_one({"url": url})
