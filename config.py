73f5e6c67cb05a72accf2c6f949f74eeeb68fb15
Ritesh <ritesh.patel@daffodilsw.com>
2023-05-16 15:56:14 +0530

    config

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 MAIL_API_KEY = os.getenv("MAIL_API_KEY")
 MONGO_URI = os.getenv("MONGO_URI")
-MONGO_URI = 'mongodb://127.0.0.1/27017'
+# MONGO_URI = 'mongodb://127.0.0.1/27017'
 MONGO_DATABASE = "cutit"
